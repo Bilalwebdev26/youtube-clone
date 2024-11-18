9c110f021d2eee1798752790529c4c3a04a0405c
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-11-17 23:57:26 -0800

    add .env file

--- Yt-clone/src/Constants/youtube.js
+++ Yt-clone/src/Constants/youtube.js
@@ -1,3 +1,3 @@
 export const base_URL="https://www.googleapis.com/youtube/v3"
-export const API_key = "AIzaSyBwknleM1bkpCUD2SD3xxjD2zYXSA3yxt8"
-export const YOUTUBE_VIDEO_API=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${API_key}`
+export const API_key = ""
+export const YOUTUBE_VIDEO_API=``
